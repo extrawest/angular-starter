8812fa05a99213dc08a08d65d9b8e8e576c77054
Andrii Zhyhariev <andrii.zhyhariev@extrawest.com>
2023-03-18 17:43:36 +0200

    feat: first implementation of new git for developer

--- firstConfig.js
+++ firstConfig.js
@@ -1,8 +1,29 @@
 import { execSync } from 'child_process';
+import rimraf from 'rimraf';
+
+const executeCommandInTerminal = (command) =>
+  execSync(command, { stdio: 'inherit' });
 
 console.log(
   'This is config script for the first initialization of new project.',
 );
 
-execSync('npm install', { stdio: [0, 1, 2] });
-execSync('npm run configure-husky', { stdio: [0, 1, 2] });
+// Install modules
+executeCommandInTerminal('npm install');
+
+// Configure husky
+executeCommandInTerminal('npm run configure-husky');
+
+// Remove config dirs
+rimraf.sync('.vscode');
+rimraf.sync('.idea');
+rimraf.sync('.husky');
+rimraf.sync('.angular');
+
+// Remove current git dir
+rimraf.sync('.git');
+
+// Init new git
+executeCommandInTerminal(
+  'git init && git add . && git commit -m "initial commit"',
+);
