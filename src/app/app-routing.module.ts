be22de1f3bfe6ee1c0e62ab0a1389f0888bd6190
Andrii Zhyhariev <andrii.zhyhariev@extrawest.com>
2023-03-19 14:45:30 +0200

    feat: created dummy auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './shared/layout/layout.component';
+import { AuthGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   {
     path: '',
     component: LayoutComponent,
     children: [
       {
         path: 'dashboard',
+        canActivate: [AuthGuard],
         loadChildren: () =>
           import('./feature/dashboard/dashboard.module').then(
             (m) => m.DashboardModule,
@@ -31,7 +33,7 @@ const routes: Routes = [
       ),
   },
 
-  { path: '**', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '**', redirectTo: 'login', pathMatch: 'full' },
 ];
 
 @NgModule({
