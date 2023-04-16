aee7b455fe77592d5999bc79f1403203a14c8cbf
Andrii Zhyhariev <andrii.zhyhariev@extrawest.com>
2023-04-16 17:28:33 +0300

    feat: migrate for bootstrap and app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [RouterOutlet, RouterLink, TranslateModule],
 })
 export class AppComponent {
   constructor(private translate: TranslateService) {
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { environment } from './environments/environment';
+import { AppComponent } from './app/app.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { AuthInterceptor } from './app/core/interceptors/auth.interceptor';
+import {
+  HTTP_INTERCEPTORS,
+  withInterceptorsFromDi,
+  provideHttpClient,
+  HttpClient,
+} from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './app/shared/layout/layout.component';
+import { AuthGuard } from './app/core/guards/auth.guard';
+import { mockResolverResolver } from './app/core/resolvers/mock-resolver.resolver';
 
 if (environment.production) {
   enableProdMode();
@@ -12,6 +25,65 @@ if (environment.production) {
   console.warn = (): void => {};
 }
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
+function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
+
+const mainRoutes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'dashboard',
+        canActivate: [AuthGuard],
+        resolve: {
+          example: mockResolverResolver,
+        },
+        loadChildren: () =>
+          import('./app/feature/dashboard/dashboard.module').then(
+            (m) => m.DashboardModule,
+          ),
+      },
+    ],
+  },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./app/feature/login/login.module').then((m) => m.LoginModule),
+  },
+
+  {
+    path: 'register',
+    loadChildren: () =>
+      import('./app/feature/register/register.module').then(
+        (m) => m.RegisterModule,
+      ),
+  },
+
+  { path: '**', redirectTo: 'login', pathMatch: 'full' },
+];
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(
+      BrowserModule,
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient],
+        },
+      }),
+      RouterModule.forRoot(mainRoutes),
+    ),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+    provideHttpClient(withInterceptorsFromDi()),
+    provideAnimations(),
+  ],
+}).catch((err) => console.error(err));
