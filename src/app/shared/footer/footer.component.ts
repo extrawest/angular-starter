a1c7a0149d4ebe3c04924d64e727d940bab0e62a
Andrii Zhyhariev <andrii.zhyhariev@extrawest.com>
2023-05-21 19:34:20 +0300

    feat: footer component with copyright text

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { ToolbarModule } from 'primeng/toolbar';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
   standalone: true,
+  imports: [ToolbarModule, TranslateModule],
 })
 export class FooterComponent {
   constructor() {}
