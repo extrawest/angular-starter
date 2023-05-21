97574d46171ebf816b2a950d830c9b6cd5aaa2f3
Andrii Zhyhariev <andrii.zhyhariev@extrawest.com>
2023-05-21 14:40:27 +0300

    feat: dumy login page

--- src/app/feature/login/login.component.ts
+++ src/app/feature/login/login.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
-    selector: 'app-login',
-    templateUrl: './login.component.html',
-    styleUrls: ['./login.component.scss'],
-    standalone: true
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+  standalone: true,
+  imports: [ReactiveFormsModule, PasswordModule, ButtonModule],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  form!: FormGroup;
+  isSaving: boolean = false;
+
   constructor() {}
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      login: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+    });
+  }
+
+  // TODO: use rxjs here
+  onSubmit(): void {
+    this.isSaving = true;
+
+    setTimeout(() => {
+      console.log(this.form.getRawValue());
+      this.isSaving = false;
+    }, 2_000);
+  }
 }
